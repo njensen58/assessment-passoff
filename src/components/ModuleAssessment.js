b01fbb96598671f1cba775c4ff9a84946cd3054f
Nate Jensen <nathan.jensen.58@gmail.com>
2019-10-08 12:01:20 -0600

    changed data organization to individual json files per module

--- src/components/ModuleAssessment.js
+++ src/components/ModuleAssessment.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import SelectForm from './SelectForm.js'
 import Assessment from './Assessment.js'
-import { modules } from '../data/moduleData.json'
+import { module0 } from '../data/module-0.json'
+import { module1 } from '../data/module-1.json'
+import { module2 } from '../data/module-2.json'
+
 
 const ModuleAssessment = () => {
   const [ selectedModule, setSelectedModule ] = useState({})
 
   const handleModuleSelect = m => {
-    if(m.module === "module-0")      setSelectedModule({...modules["module-0"], title: m.module})
-    else if(m.module === "module-1") setSelectedModule({...modules["module-1"], title: m.module})
-    else if(m.module === "module-2") setSelectedModule({...modules["module-2"], title: m.module})
+    if(m.module === "module-0")      setSelectedModule({...module0, title: m.module})
+    else if(m.module === "module-1") setSelectedModule({...module1, title: m.module})
+    else if(m.module === "module-2") setSelectedModule({...module2, title: m.module})
   }
 
   return (
